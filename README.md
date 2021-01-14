6fb688c81b6fdb1978525a24e9afe5fc36bf9a65
Julien Verdun <verdun.julien@yahoo.fr>
2021-01-14 16:03:28 +0100

    Add gamma correction, shadows, mirror and transparent surfaces

--- README.md
+++ README.md
@@ -92,3 +92,120 @@ Le résultat obtenu est présenté sur la Figure ci-dessous :
 
 - les ombres des sphères ne sont pas projetées sur les murs alentours
 - la luminosité de la lumière sur les murs est très faible, leur couleur n'est pas très bien perceptible.
+
+### Correction Gamma
+
+On se propose ici d'améliorer le contraste des objets. En effet, sur la dernière scène obtenue, nous avons relevé la faible luminosité des murs en arrière plan.
+
+La **correction Gamma** permet d'améliorer le contraste en réalisant une opération simple sur l'intensité lumineuse calculée de chaque pixel :
+
+I = I<sub>calculee</sub><sup>1/&gamma;</sup>
+
+avec &gamma; = 2,2 et 1/&gamma; = 0,45.
+
+La correction Gamma permet d'obtenir le résultat ci-dessous. La luminosité des pixels représentant les murs est plus importante. Le rendu est meilleur.
+
+![raytracer_correction_gamma](Figures\raytracer_correction_gamma.png)
+
+### Ombres portées
+
+On souhaite encore améliorer le rendu en ajoutant les **ombres portées**, c'est-à-dire les ombres des objets projetées sur les surfaces.
+
+La stratégie est la suivante. On envoie des rayons dans toutes les directions (vers chaque pixel de l'écran). Pour un rayon donné, on trouve l'intersection avec un objet de la scène puis on regarde si le rayon entre cette intersection et la source de lumière coupe un autre objet. Si tel est le cas, on rend le pixel d'intersection plus sombre.
+
+On obtient un premier résultat présenté sur la Figure ci-dessous. On remarque que les ombres sont bien présentes, toutefois, l'image est très bruitée.
+
+![raytracer_shadow_and_noise](Figures\raytracer_shadow_and_noise.png)
+
+Le bruit s'explique par les incertitudes de calcul dû à la précision numérique de la machine. Ainsi, le point d'intersection se retrouve dans la surface au lieu d'être sur la surface. Pour éviter cela, on déplace le point d'intersection d'une petite quantité &epsilon; vers l'extérieur de l'objet.
+
+Avec cette légère modification, on obtient le résultat présenté ci-dessous, le bruit n'est plus présent et les ombres sont bien visibles.
+
+![raytracer_avec_shadow](Figures\raytracer_avec_shadow.png)
+
+### Surfaces miroir
+
+Les surfaces représentées jusqu'à présent sont des surfaces opaques qui possèdent un albédo. On se propose ici de représenter un autre type de surface, les **surfaces miroir**.
+
+Un rayon lumineux se reflète sur une telle surface avec un angle de réflexion par rapport à la normale de la surface égal à l'angle d'incidence. Les réflexions peuvent être multiples dans le cas de plusieurs surfaces miroir.
+
+On implémente ces types de surfaces en créant une fonction getColor récursive qui permet de donner la couleur entre l'intersection d'un rayon et d'une surface opaque, ou le cas écheant entre le rayon réfléchi et une surface miroir.
+
+Avec cette propriété, notre **raytracer** devient un **pathtracer**.
+
+Le rayon incident **i** est réfléchi par la surface miroir en un rayon **r** tel que :
+
+**r** = **i** - 2<**i**,**N**>**N**
+
+On remplace la sphère centrale des scènes précédentes par une surface miroir. On obtient le résultat ci-dessous.
+
+![raytracer_mirror_noise](Figures\raytracer_mirror_noise.png)
+
+On rencontre le même problème que précédemment, du bruit est présent sur la surface dû aux erreurs numériques. Le résultat après correction est le suivant.
+
+![raytracer_mirror](Figures\raytracer_mirror.png)
+
+La sphère centrale permet bien de refléter le mur derrière l'écran, le mur magenta et les sphères et murs environnants.
+
+### Surfaces transparents
+
+On se propose ici de représenter un autre type de surface, les **surfaces transparentes**.
+
+On fait l'hypothèse qu'une surface transparente réfléchie l'intégralité d'un rayon ou transmet l'intégralité du rayon, on ne tient pas compte de la **transmission de Fresnel**.
+
+On utilise la **loi de Snell-Descartes**, pour une surface de normale **N** séparant deux milieux d'indices respectifs n<sub>1</sub> et n<sub>2</sub>, un rayon incident dans le milieu d'indice n<sub>1</sub> avec un angle &theta;<sub>i</sub> est transmis dans le milieu d'indice n<sub>2</sub> avec un angle &theta;<sub>t</sub> tel que :
+
+n<sub>1</sub> sin(&theta;<sub>i</sub>) = n<sub>2</sub> sin(&theta;<sub>t</sub>)
+
+Grâce à cette loi, on peut exprimer les composantes tangentielle et normale du vecteur unitaire directeur du rayon transmis **T** :
+
+**T<sub>N</sub>** = - sqrt(1 - (n<sub>1</sub>/n<sub>2</sub>)<sup>2</sup>(1-<**i**,**N**><sup>2</sup>)) **N**
+
+**T<sub>T</sub>** = n<sub>1</sub>/n<sub>2</sub> (**i** - <**i**,**N**>**N**)
+
+Connaissant la direction du rayon tranmis par la surface transarente, il est alors possible de connaître la couleure du pixel à afficher en cherchant l'intersection de ce rayon avec le reste de la scène.
+
+On modifie la sphère de droite (sphère rouge) en une surface transparente. On obtient le résultat présenté sur la Figure ci-dessous. La sphère est bien transparente et laisse apparaître avec une inversion de la direction les murs de droite et du fond.
+
+![raytracer_transparent](Figures\raytracer_transparent.png)
+
+Une autre représentation est visible ci-dessous en remplaçant le sol par un miroir.
+On calcule le temps nécessaire pour créer cette scène. L'algorithme donne les performances suivantes :
+
+- 0.918 seconde pour générer l'image sans le sol en miroir
+- 1.137 secondes pour générer l'image avec le sol en miroir.
+
+![raytracer_transparent_sol_mirroir](Figures\raytracer_transparent_sol_mirroir.png)
+
+### Transmission de Fresnel
+
+Lors de la transmission d'un rayon par une surface transparente, une partie du rayon est en réalité réfléchie par la surface, selon les lois de Fresnel.
+
+Dans l'approximation de Schlick, le rayon incident se divise en un rayon réfléchi **R** et un rayon transmis **T** selon les formules suivantes :
+
+R = k<sub>0</sub> + (1-k<sub>0</sub>)(1-|<**N**,**i**>|)<sup>5</sup>
+
+T = 1 - R
+
+avec k<sub>0</sub> = (n<sub>1</sub>-n<sub>2</sub>)<sup>2</sup>/(n<sub>1</sub>+n<sub>2</sub>)<sup>2</sup>
+
+Pour implémenter la transmission de Fresnel, deux stratégies ont été testées.
+
+La **première stratégie** consiste à calculer la valeur d'un pixel transparent en faisant une moyenne pondérée par les coefficients R et T du rayon réfléchi et du rayon réfracté. L'inconvénient de cette méthode est son temps de calcul. En effet, la méthode de calcul de la couleur étant récursive, l'appeler 2 fois pour calculer la valeur d'un pixel transparent conduit à fait 2<sup>N</sup> calcul pour N rebonds, la complexité devient très vite importante.
+
+Les résultats de cette méthode sont très concluants puisqu'en moins de 2 secondes, les deux images ci-dessous sont générées avec un rendu proche de la réalité.
+
+![raytracer_fresnel_gourmand](Figures\raytracer_fresnel_gourmand.png)
+
+![raytracer_fresnel_gourmand_sol_mirror](Figures\raytracer_fresnel_gourmand_sol_mirror.png)
+
+La **deuxième stratégie** consiste à calculer la valeur d'un pixel transparent en faisant une moyenne de plusieurs chemins complets pour lesquels à chaque intersection avec une surface transparente, on choisit aléatoirement d'émettre un rayon réfléchi ou réfracté, en générant un nombre entre 0 et 1, et en le comparant au coefficient R. Si le nombre généré est inférieur à R, le rayon sera réfléchi, sinon il sera réfracté.
+
+Les résultats de cette méthode semble un peu moins performant. en effet, en générant et moyennant la valeur du pixel pour 30 rayons complets, on obtient le résultat ci-dessous. La qualité de la transparence est moins importante qu'avec la première méthode, on observe un léger bruit. De plus, le temps pour générer l'image est plus grand avec environ 6 secondes pour 10 rayons et 20 secondes pour 30 rayons.
+
+![raytracer_fresnel_30_tirages](Figures\raytracer_fresnel_30_tirages.png)
+
+# TO-DO :
+
+- mettre un feedback sur le cours (si a aimé, si deja codé en c++, si c'était difficile, intéressant, etc)
+- faire une table des matières
--- raytracer.cpp
+++ raytracer.cpp
@@ -9,6 +9,9 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+#include <ctime>
+#include <fstream>
+
 #define M_PI 3.14159265358979323846
 
 class Vector
@@ -46,6 +49,11 @@ Vector operator-(const Vector &a, const Vector &b)
     return Vector(a[0] - b[0], a[1] - b[1], a[2] - b[2]);
 }
 
+Vector operator-(const Vector &a)
+{
+    return Vector(-a[0], -a[1], -a[2]);
+}
+
 Vector operator*(double a, const Vector &b)
 {
     return Vector(a * b[0], a * b[1], a * b[2]);
@@ -66,6 +74,11 @@ double dot(const Vector &a, const Vector &b)
     return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
 }
 
+double sqr(double x)
+{
+    return x * x;
+}
+
 class Ray
 {
 public:
@@ -79,7 +92,7 @@ public:
 class Sphere
 {
 public:
-    Sphere(const Vector &O, double R, const Vector &albedo) : O(O), R(R), albedo(albedo)
+    Sphere(const Vector &O, double R, const Vector &albedo, bool isMirror = false, bool isTransparent = false) : O(O), R(R), albedo(albedo), isMirror(isMirror), isTransparent(isTransparent)
     {
     }
     bool intersect(const Ray &r, Vector &P, Vector &N, double &t)
@@ -120,6 +133,7 @@ public:
     Vector O;
     double R;
     Vector albedo;
+    bool isMirror, isTransparent;
 };
 
 class Scene
@@ -127,9 +141,11 @@ class Scene
 public:
     Scene(){};
     std::vector<Sphere> objects;
-    bool intersect(const Ray &r, Vector &P, Vector &N, Vector &albedo)
+    Vector L;
+    double I;
+    bool intersect(const Ray &r, Vector &P, Vector &N, Vector &albedo, bool &mirror, bool &transp, double &t)
     {
-        double t = 1E10;
+        t = 1E10;
         bool has_inter = false;
         for (int i = 0; i < objects.size(); i++)
         {
@@ -142,29 +158,129 @@ public:
                 albedo = objects[i].albedo;
                 P = localP;
                 N = localN;
+                mirror = objects[i].isMirror;
+                transp = objects[i].isTransparent;
             }
         }
         return has_inter;
     };
+
+    Vector getColor(const Ray &r, int rebond)
+    {
+        double epsilon = 0.00001;
+        Vector P, N, albedo;
+        double t;
+        bool mirror, transp;
+        bool inter = intersect(r, P, N, albedo, mirror, transp, t);
+        Vector color(0, 0, 0);
+        if (rebond > 5)
+            return Vector(0., 0., 0.);
+        if (inter)
+        {
+            if (mirror)
+            {
+                Vector reflectedDir = r.u - 2 * dot(r.u, N) * N;
+                Ray reflectedRay(P + epsilon * N, reflectedDir);
+                return getColor(reflectedRay, rebond + 1);
+            }
+            else
+            {
+                if (transp)
+                {
+                    double n1 = 1, n2 = 1.4;
+                    Vector N2 = N;
+                    if (dot(r.u, N) > 0)
+                    { //on sort de la sphère
+                        std::swap(n1, n2);
+                        N2 = -N;
+                    }
+
+                    double rad = 1 - sqr(n1 / n2) * (1 - sqr(dot(r.u, N2)));
+
+                    if (rad < 0) //rayon rasant, i-e rayon reflechie uniquement
+                    {
+                        Vector reflectedDir = r.u - 2 * dot(r.u, N) * N;
+                        Ray reflectedRay(P + epsilon * N, reflectedDir);
+                        return getColor(reflectedRay, rebond + 1);
+                    }
+
+                    double k0 = sqr(n1 - n2) / sqr(n1 + n2);
+
+                    double R = k0 + (1 - k0) * sqr(sqr(1 - std::abs(dot(N2, r.u)))) * (1 - std::abs(dot(N2, r.u)));
+
+                    // implementation de la transmission de Fresnel avec tirage aleatoire et moyenne
+                    // double random = rand() % 100;
+
+                    // if (random <= 100 * R)
+                    // {
+                    //     Vector reflectedDir = r.u - 2 * dot(r.u, N) * N;
+                    //     Ray reflectedRay(P + epsilon * N, reflectedDir);
+                    //     return getColor(reflectedRay, rebond + 1);
+                    // }
+                    // else
+                    // {
+                    //     // double T = 1 - R;
+                    //     Vector tT = n1 / n2 * (r.u - dot(r.u, N2) * N2);
+                    //     Vector tN = -sqrt(rad) * N2;
+                    //     Vector refractedDir = tT + tN;
+                    //     return getColor(Ray(P - epsilon * N2, refractedDir), rebond + 1);
+                    // }
+
+                    Vector reflectedDir = r.u - 2 * dot(r.u, N) * N;
+                    Ray reflectedRay(P + epsilon * N, reflectedDir);
+
+                    double T = 1 - R;
+
+                    Vector tT = n1 / n2 * (r.u - dot(r.u, N2) * N2);
+                    Vector tN = -sqrt(rad) * N2;
+                    Vector refractedDir = tT + tN;
+
+                    // sans transmission de Fresnel
+                    // return getColor(Ray(P - epsilon * N2, refractedDir), rebond + 1);
+
+                    // avec transmission de Fresnel
+                    return (T * getColor(Ray(P - epsilon * N2, refractedDir), rebond + 1) + R * getColor(reflectedRay, rebond + 1));
+                }
+                else
+                {
+                    Vector PL = L - P;
+                    double d = sqrt(PL.sqrNorm());
+                    Vector shadowP, shadowN, shadowAlbedo;
+                    double shadowt;
+                    bool shadowMirror, shadowTransp;
+                    Ray shadowRay(P + 0.00001 * N, PL / d);
+                    bool shadowInter = intersect(shadowRay, shadowP, shadowN, shadowAlbedo, shadowMirror, shadowTransp, shadowt);
+                    if (shadowInter && shadowt < d)
+                    {
+                        color = Vector(0., 0., 0.);
+                    }
+                    else
+                    {
+                        color = (I / (4 * M_PI * d * d)) * (albedo / M_PI) * std::max(0., dot(N, PL / d));
+                    }
+                }
+            }
+        }
+        return color;
+    }
 };
 
 int main()
 {
+    float ini_time = clock();
     int W = 512;
     int H = 512;
 
     Vector C(0, 0, 55);
     Scene scene;
-    // Vector O(0, 0, 0);
-    // double R = 10;
     Sphere S1(Vector(-15, 0, 0), 10, Vector(0., 0., 1.));
-    Sphere S2(Vector(0, 0, 0), 10, Vector(1., 1., 1.));
-    Sphere S3(Vector(15, 0, 0), 10, Vector(1., 0., 0.));
+    Sphere S2(Vector(0, 0, 0), 10, Vector(1., 1., 1.), true);
+    Sphere S3(Vector(15, 0, 0), 10, Vector(1., 0., 0.), false, true);
     Sphere Smurga(Vector(-1000, 0, 0), 970, Vector(0., 0., 1.));
     Sphere Smurdr(Vector(1000, 0, 0), 970, Vector(1., 0., 0.));
     Sphere Smurfa(Vector(0, 0, -1000), 940, Vector(0., 1., 0.));
     Sphere Smurde(Vector(0, 0, 1000), 940, Vector(1., 0., 1.));
-    Sphere Ssol(Vector(0, -1000, 0), 990, Vector(1., 1., 1.));
+    Sphere Ssol(Vector(0, -1000, 0), 990, Vector(1., 1., 1.), true);
     Sphere Splafond(Vector(0, 1000, 0), 990, Vector(1., 1., 1.));
     scene.objects.push_back(S1);
     scene.objects.push_back(S2);
@@ -177,9 +293,8 @@ int main()
     // scene.objects.push_back(Splafond);
 
     double fov = 60 * M_PI / 180;
-    double I = 1E7;
-    // Vector rho(1, 0, 0);
-    Vector L(-10, 20, 40);
+    scene.I = 5E9;
+    scene.L = Vector(-10, 20, 40);
 
     std::vector<unsigned char> image(W * H * 3, 0);
     for (int i = 0; i < H; i++)
@@ -189,24 +304,32 @@ int main()
             Vector u(j - W / 2, i - H / 2, -W / (2. * tan(fov / 2)));
             u = u.get_normalized();
             Ray r(C, u);
-            Vector P, N, albedo;
-            bool inter = scene.intersect(r, P, N, albedo);
-            Vector color(0, 0, 0);
-            if (inter)
-            {
-                Vector PL = L - P;
-                double d = sqrt(PL.sqrNorm());
-                // double ddot = dot(N, PL / d);
-                // color = Vector(255, 255, 255);
-                color = (I / (4 * M_PI * d * d)) * (albedo / M_PI) * std::max(0., dot(N, PL / d));
-            }
 
-            image[((H - i - 1) * W + j) * 3 + 0] = std::min(255., color[0]);
-            image[((H - i - 1) * W + j) * 3 + 1] = std::min(255., color[1]);
-            image[((H - i - 1) * W + j) * 3 + 2] = std::min(255., color[2]);
+            Vector color = scene.getColor(r, 0);
+
+            // implementation de la transmission de Fresnel avec tirage aleatoire et moyenne
+            // Vector color = Vector(0., 0., 0.);
+            // int nbVal = 30;
+            // for (int i = 0; i < nbVal; i++)
+            // {
+            //     color = color + scene.getColor(r, 0);
+            // }
+            // color = color / nbVal;
+
+            image[((H - i - 1) * W + j) * 3 + 0] = std::min(255., std::pow(color[0], 0.45));
+            image[((H - i - 1) * W + j) * 3 + 1] = std::min(255., std::pow(color[1], 0.45));
+            image[((H - i - 1) * W + j) * 3 + 2] = std::min(255., std::pow(color[2], 0.45));
         }
     }
+
+    // wwriting the image on a png file
     stbi_write_png("raytracer.png", W, H, 3, &image[0], 0);
+
+    // writing execution time on a txt file
+    std::ofstream execution_file;
+    execution_file.open("execution_time.txt");
+    execution_file << (clock() - ini_time) / CLOCKS_PER_SEC;
+    execution_file.close();
 
     return 0;
 };
